3402dba256b88008583099e538da799847a6bf44
Affodilaj Fadil <fadhilaainsalsabilla@mail.ugm.ac.id>
2023-08-29 06:14:31 -0700

    pertemuan 3, August 29

--- webPoject1/routes/web.php
+++ webPoject1/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Larissa Tanaya",
+        "email" => "lalrissa@gmail.com"
+    ]);
+});
+
+
+
+
+
